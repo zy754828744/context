bff16455068b61d6ed32d4db89d0cef93d73dd9c
LAPTOP-7K8SUFP0\张勇 <754828744@qq.com>
2018-02-09 22:06:32 +0800

    no message

--- context2/src/res/context/config/RootConfig.java
+++ context2/src/res/context/config/RootConfig.java
@@ -2,10 +2,9 @@ package context.config;
 
 import context.ComponetScanEmptyInterface;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -15,14 +14,22 @@ import javax.sql.DataSource;
 @Configuration
 @ComponentScan(basePackageClasses = ComponetScanEmptyInterface.class
 ,excludeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION,value = EnableWebMvc.class)})
+@PropertySource("classpath:/context/dao.properties")
 public class RootConfig {
+
+    @Autowired
+    private Environment environment;
     @Bean
     public DataSource getDataSource(){
+        System.out.println(environment.getProperty("driver"));
+        System.out.println(environment.getProperty("url"));
+        System.out.println(environment.getProperty("user"));
+        System.out.println(environment.getProperty("password"));
         DriverManagerDataSource ds=new DriverManagerDataSource();
-        ds.setDriverClassName("com.mysql.jdbc.Driver");
-        ds.setUrl("jdbc:mysql://localhost:3306/context");
-        ds.setUsername("root");
-        ds.setPassword("1995zy");
+        ds.setDriverClassName(environment.getProperty("driver"));
+        ds.setUrl(environment.getProperty("url"));
+        ds.setUsername(environment.getProperty("user"));
+        ds.setPassword(environment.getProperty("password"));
         return ds;
     }
 
